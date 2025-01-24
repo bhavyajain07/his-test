fb3939c24b102e8d977e4bf61894767ab5b91ba7
bhavyajain07 <bjjain335@gmail.com>
2025-01-24 19:24:16 +0530

    minor bug fixes in settings (spelling errors)

--- demo/settings.py
+++ demo/settings.py
@@ -137,7 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 
 # Direct credential assignment (for development only)
-AWL_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY")
+AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY")
 AWS_SECRET_KEY = os.environ.get("AWS_SECRET_KEY")
 AWS_REGION = os.environ.get("AWS_REGION")
 AWS_BUCKET_NAME = os.environ.get("AWS_BUCKET_NAME")
